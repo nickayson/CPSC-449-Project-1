bb5c6771a7263b35e632395d8f64aa53a2a1e93e
Debdyuti <das.debdyuti01@gmail.com>
2022-10-16 11:56:57 -0700

    Added error cases

--- userService.py
+++ userService.py
@@ -2,7 +2,7 @@ import dataclasses
 import databases
 from quart import Quart, g, request, jsonify, abort
 from werkzeug.security import generate_password_hash, check_password_hash
-from quart_schema import validate_request
+from quart_schema import validate_request, RequestSchemaValidationError
 import sqlite3
 
 app = Quart(__name__)
@@ -33,64 +33,15 @@ async def all_users():
 
     return list(map(dict, all_users))  
 
-
-
-# #Authenticating the user
-
-# @app.route('/login', methods=['POST'])
-# async def autenticateUserCredentials():
-#         query_parameters = request.form
-
-#         username = await query_parameters.get('username')
-#         password = await query_parameters.get('password')
-
-#         db = _get_db()
-#         #result = query_db('SELECT password FROM users WHERE username = ?', [username])
-#         result=  db.fetch_one("SELECT password FROM userData WHERE username =:username", values={"username": username})
-
-#         hashed_password = result[0].get('password')
-#         validate_user = check_password_hash(hashed_password, password)    #checking if user entered password is equal to the hashed password in db
-#         if validate_user:
-#             return jsonify(validate_user)
-#         else:
-#             return jsonify({"statusCode": 401, "error": "Unauthorized", "message": "Login failed: Invalid username or password" })
-
-
-
-
-
-# # Registering a new user
-
-# @app.route('/register', methods=['POST'])
-# async def registerUser():
-#        query_parameters = request.form
-#        id = query_parameters.get('id')
-#        username = query_parameters.get('username')
-#        password = query_parameters.get('password')
-#        hashed_password = generate_password_hash(password, "sha256")
-
-#        app.logger.info(username)
-
-#        db = await _get_db()
-#        count = db.fetch_one('SELECT COUNT(*) as count FROM userData WHERE id = ?', [id])
-#        if(count[0].get('count') > 0):                             #returns 400 error if username already exists
-#           return jsonify({"statusCode": 400, "error": "Bad Request", "message": "Username already taken"})
-#        elif username== None or username=='' or password== None or password=='': #returns 400 error if either username or password or email are not provided.
-#           return jsonify({"statusCode": 400, "error": "Bad Request", "message": "Invalid parameter(s)"})
-#        else:                                                      #new user successful registration
-#           db.execute('INSERT INTO userData (id, username, password) VALUES(?,?,?)',(id, username, hashed_password))
-#           res = db.commit()
-#           return jsonify({"statusCode": 200, "message": "You have successfully registered!"})
-
-
-
-
 #Registering a new user
 @app.route("/register/", methods=["POST"])
 @validate_request(userData)
-async def create_user(data):
+async def register_user(data):
     db = await _get_db()
-    userData = dataclasses.asdict(data)
+    
+    
+    userData = dataclasses.asdict(data)   
+    
     try:
         id = await db.execute(
             """
@@ -102,5 +53,14 @@ async def create_user(data):
     except sqlite3.IntegrityError as e:
         abort(409, e)
     
-    userData["id"] = id    
+    userData["id"] = id      
     return userData, 201, {"Location": f"/user/{id}"}
+
+@app.errorhandler(RequestSchemaValidationError)
+def bad_request(e):
+    return {"error": str(e.validation_error)}, 400
+
+@app.errorhandler(409)
+def conflict(e):
+    return {"error": str(e)}, 409
+
