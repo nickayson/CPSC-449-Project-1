8158c389870ea6de1028386a4702bc7c309b25a4
Debdyuti <das.debdyuti01@gmail.com>
2022-10-17 19:14:33 -0700

    Added Login method

--- userService.py
+++ userService.py
@@ -1,4 +1,7 @@
 import dataclasses
+import collections
+from this import d
+from operator import itemgetter
 import databases
 from quart import Quart, g, request, jsonify, abort
 from werkzeug.security import generate_password_hash, check_password_hash
@@ -10,6 +13,11 @@ app = Quart(__name__)
 @dataclasses.dataclass
 class userData:
     id: int
+    username: str
+    password: str
+
+@dataclasses.dataclass
+class loginData:    
     username: str
     password: str
 
@@ -33,12 +41,13 @@ async def all_users():
 
     return list(map(dict, all_users))  
 
-#Registering a new user
+
+#------------Registering a new user-----------------#
+
 @app.route("/register/", methods=["POST"])
 @validate_request(userData)
 async def register_user(data):
-    db = await _get_db()
-    
+    db = await _get_db()  
     
     userData = dataclasses.asdict(data)   
     
@@ -64,3 +73,71 @@ def bad_request(e):
 def conflict(e):
     return {"error": str(e)}, 409
 
+@app.errorhandler(401)
+def unauthorized(e):
+    return {"error": str(e)}, 401
+
+
+SearchParam = collections.namedtuple("SearchParam", ["name", "operator"])
+SEARCH_PARAMS = [
+    
+    SearchParam(
+        "username",
+        "=",
+    ),
+    SearchParam(
+        "password",
+        "=",
+    ),
+    
+]
+
+#-------------Authenticating the credentials for Login----------------
+@app.route('/login', methods=['POST'])
+@validate_request(loginData)
+async def authenticate(data):
+    query_parameters = request.args
+    db = await _get_db()   
+    
+    loginData= dataclasses.asdict(data)   
+    
+    sql = "SELECT username,password FROM userData"
+    conditions = []
+    values = {}
+
+    for param in SEARCH_PARAMS:
+        if query_parameters.get(param.name):
+            if param.operator == "=":
+                conditions.append(f"{param.name} = :{param.name}")
+                values[param.name] = query_parameters[param.name]               
+
+    if conditions:
+        sql += " WHERE "
+        sql += " AND ".join(conditions)
+
+    app.logger.debug(sql)
+
+    db = await _get_db()
+    results = await db.fetch_all(sql, values)    
+    
+    my_list= list(map(dict, results))    
+    pwd = list(map(itemgetter('password'), my_list))
+    name = list(map(itemgetter('username'), my_list))    
+   
+    result_dict= {}
+
+    for key in name:
+         for value in pwd:
+             result_dict[key] = value
+             pwd.remove(value)
+             break      
+    
+    for key in result_dict:
+        if(loginData['password']==result_dict[key] and loginData['username']==key  ) :        
+            return jsonify({"statusCode": 200, "authenticated": "true"})   
+     
+    return jsonify({"statusCode": 401, "error": "Unauthorized", "message": "Login failed !" })     
+   
+    
+   
+   
